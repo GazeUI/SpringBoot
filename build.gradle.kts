8a8441d8437e179de547684ca070a4b254838ad0
Rosberg Linhares <rosberglinhares@gmail.com>
2019-10-29 02:44:57 +0100

    Require JDK 8+
    The idea is require the same required version of Spring Boot (https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#getting-started-system-requirements), so that Spring Boot developers stay safe in using this library.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,18 @@ plugins {
     `java-library`
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    // Related issues:
+    //   - Gradle: https://github.com/gradle/gradle/issues/2510
+    //   - Eclipse: https://github.com/eclipse/buildship/issues/934
+    options.compilerArgs.addAll(arrayOf("--release", "8"))
+}
+
 repositories {
     jcenter()
 }
